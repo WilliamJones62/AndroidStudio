5b99404dd49d137c89739501465720dc1a8aa195
williamjones62 <williamjones62@gmail.com>
2017-07-03 08:09:41 -0400

    Added basic variables and arrays to app.

--- WillowTreeQuiz/app/src/main/java/com/bignerdranch/willowtreequiz/MainActivity.java
+++ WillowTreeQuiz/app/src/main/java/com/bignerdranch/willowtreequiz/MainActivity.java
@@ -1,4 +1,4 @@
-package com.bignerdranch.geoquiz;
+package com.bignerdranch.willowtreequiz;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -9,35 +9,55 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class QuizActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private TextView mQuestion;
+    private boolean answers[] = {true, true, false, true, false};
+    private String questions[] = {  "WillowTree has offices in Charlottesville, Durham and New York City",
+                                    "WillowTree designs and develops mobile and web apps for the world's leading brands",
+                                    "WillowTree is not a place for Makers",
+                                    "At WillowTree we have lots of fun doing work we love",
+                                    "WillowTree is not certified by Apple, Google and Microsoft"};
+    private int counter = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quiz);
+        setContentView(R.layout.activity_main);
         mTrueButton = (Button)findViewById(R.id.true_button);
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-// Wrong answer
-                Toast.makeText(QuizActivity.this,
-                        R.string.incorrect_toast,
-                        Toast.LENGTH_SHORT).show();
+                if (answers[counter] = false) {
+                    Toast.makeText(MainActivity.this,
+                            R.string.incorrect_toast,
+                            Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(MainActivity.this,
+                            R.string.correct_toast,
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
         mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-// Right answer
-                Toast.makeText(QuizActivity.this,
-                        R.string.correct_toast,
-                        Toast.LENGTH_SHORT).show();
+
+                if (answers[counter] = true) {
+                    Toast.makeText(MainActivity.this,
+                            R.string.incorrect_toast,
+                            Toast.LENGTH_SHORT).show();
+                } else{
+                    Toast.makeText(MainActivity.this,
+                            R.string.correct_toast,
+                            Toast.LENGTH_SHORT).show();
+                }
             }
         });
         mFalseButton = (Button)findViewById(R.id.false_button);
@@ -50,7 +70,7 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_quiz, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
